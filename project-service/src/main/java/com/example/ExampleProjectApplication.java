f0bbddacc4316dc4105cd548ad08b06ad0a00d1f
wakening <32231663+wakening@users.noreply.github.com>
2021-01-11 21:08:14 +0800

    add springboot1.5.x + cloud + nacos

--- project-service/src/main/java/com/example/ExampleProjectApplication.java
+++ project-service/src/main/java/com/example/ExampleProjectApplication.java
@@ -2,7 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author wakening
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ExampleProjectApplication {
 
